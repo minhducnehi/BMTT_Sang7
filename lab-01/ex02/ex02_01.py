9b5887cf84fdafb219e161dac90e76444e89ff8b
minhducnehi <minhduc942000@gmail.com>
2026-05-16 10:51:52 +0700

    [add] lab-01 ex01 ex02

---
+++ lab-01/ex02/ex02_01.py
@@ -0,0 +1,5 @@
+#nhập tên tuổi từ người dùng
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+# in thông điệp chào mừng với thông tin đã nhập
+print("Chào mừng " + ten + "! Bạn " + tuoi + " tuổi.")
